b18674f0bb0358f76b96e97c222ced3664e6cde4
This man is cool <496347560@qq.com>
2018-11-08 15:19:31 +0800

    NO.3

--- day20181108/app.js
+++ day20181108/app.js
@@ -5,7 +5,7 @@ let m1 = require('./module1');
 let m2 = require('./module2');
 
 
-console.log(m1());
+console.log(m1());//先执行函数m1---》打印，后再执行console.log(m1())   因为函数没有return值所以输出undifend
 m1();
 console.log(m2.eat());
 m2.eat();
